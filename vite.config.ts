c36dc132589ee541e0ec8281ab0dd6ef88f7a2e0
duongbx288 <duongbx183901@gmail.com>
2023-03-27 23:30:33 +0700

    change and test CLI

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      base: '/vite-ecommerce/',
+      plugins: [react()],
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      base: '/vite-ecommerce/',
+      plugins: [react()],
+    }
+  }
 })
