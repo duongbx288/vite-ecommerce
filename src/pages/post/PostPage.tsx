6ef1dfff954eb89f66dbd142716a418eb6f07216
duongbx288 <duongbx183901@gmail.com>
2023-04-03 22:38:49 +0700

    feat: dispatching thunks from components

--- src/pages/post/PostPage.tsx
+++ src/pages/post/PostPage.tsx
@@ -1,87 +1,94 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import TimeAgo from "../../components/TimeAgo";
 import { ReactionButtons } from "../../components/post/ReactionButtons";
 import { useAppSelector, useAppDispatch } from "../../hooks/reduxHooks";
-import { postAdded } from "../../store/postSlice";
-
+import { fetchPosts, postAdded } from "../../store/postSlice";
 
 const PostPage = () => {
-const [title, setTitle] = useState("");
-const [content, setContent] = useState("");
-const [userId, setUserId] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
 
-const list = useAppSelector((state) => state.post.list);
-const orderedList = list.slice().sort((a, b) => b.date.localeCompare(a.date));
+  const list = useAppSelector((state) => state.post.list);
+  const orderedList = list.slice().sort((a, b) => b.date.localeCompare(a.date));
 
-const users = useAppSelector((state) => state.users);
+  const users = useAppSelector((state) => state.users);
 
-const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
+  const postStatus = useAppSelector((state) => state.post.status);
 
-const onTitleChanged = (e) => setTitle(e.target.value);
-const onContentChanged = (e) => setContent(e.target.value);
-const onAuthorChanged = (e) => setUserId(e.target.value);
+  useEffect(() => {
+    // only fetch data when status is 'idle'
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
 
-const renderList = orderedList.map((item) => {
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
+
+  const renderList = orderedList.map((item) => {
+    return (
+      <article className="post-excerpt" key={item.id}>
+        <h3>{item.title}</h3>
+        <p className="post-content">{item.content.substring(0, 100)}</p>
+        <TimeAgo timestamp={item.date} />
+        <ReactionButtons post={item} />s
+        <Link to={`/vite-ecommerce/post/${item.id}`}>View Post</Link>
+      </article>
+    );
+  });
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postAdded(title, content, userId));
+
+      setTitle("");
+      setContent("");
+    }
+  };
+
   return (
-    <article className="post-excerpt" key={item.id}>
-      <h3>{item.title}</h3>
-      <p className="post-content">{item.content.substring(0, 100)}</p>
-      <TimeAgo timestamp={item.date} />
-      <ReactionButtons post={item} />s
-      <Link to={`/vite-ecommerce/post/${item.id}`}>View Post</Link>
-    </article>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <>Hi</>
+      <form style={{ margin: "5px" }}>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
+      {renderList}
+    </div>
   );
-});
-
-const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
-
-const usersOptions = users.map((user) => (
-  <option key={user.id} value={user.id}>
-    {user.name}
-  </option>
-));
-
-const onSavePostClicked = () => {
-  if (title && content) {
-    dispatch(postAdded(title, content, userId));
-
-    setTitle("");
-    setContent("");
-  }
 };
 
-return (
-  <div style={{ display: "flex", flexDirection: "column" }}>
-    <>Hi</>
-    <form style={{ margin: "5px" }}>
-      <label htmlFor="postTitle">Post Title:</label>
-      <input
-        type="text"
-        id="postTitle"
-        name="postTitle"
-        value={title}
-        onChange={onTitleChanged}
-      />
-      <label htmlFor="postAuthor">Author:</label>
-      <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
-        <option value=""></option>
-        {usersOptions}
-      </select>
-      <label htmlFor="postContent">Content:</label>
-      <textarea
-        id="postContent"
-        name="postContent"
-        value={content}
-        onChange={onContentChanged}
-      />
-      <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
-        Save Post
-      </button>
-    </form>
-    {renderList}
-  </div>
-);
-}
-
- export default PostPage;
+export default PostPage;
